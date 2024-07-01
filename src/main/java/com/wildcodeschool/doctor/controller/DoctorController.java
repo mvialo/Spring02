df1e6cc5178fa356d1d0b3dfa22b182b8ce7a4ad
mvialo <manuella.vialo@outlook.fr>
2024-07-01 14:37:36 +0200

    Transfert du projet sur le dépôt distant

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -4,14 +4,23 @@ import com.wildcodeschool.doctor.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Object doctor(@PathVariable int number) {
+        if (number == 13){
+            return new Doctor(13,"Jodie Whittaker");
+        }
+        else if ( number >= 1 && number <= 12 ){
+            return "Erreur 303";
+        }
+        else{
+            return "Erreur 404 \nImpossible de récupérer l'incarnation "+ number;
+        }
+        
     }
 }
